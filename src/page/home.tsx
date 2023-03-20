fc8bb1891b70e8ea0926d0a54f1bdf92d838b803
admin <2642438745@qq.com>
2023-03-20 23:59:27 +0800

    feat: account

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,17 +1,36 @@
-import { userInfo } from 'os';
+
 import React, { useEffect, useState } from 'react';
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 import { connect } from 'react-redux';
+import { number } from 'prop-types';
+interface accountInt {
+  avatar: string,
+  nickname: string,
+  gender: number,
+}
 
 const Home = (props: any) => {
-  const [account, setAccount] = useState({ nickname: '', age: 0 });
+  const [account, setAccount] = useState<accountInt>({
+    avatar: '',
+    nickname: '',
+    gender: 0,
+  });
   useEffect(() => {
     const userInfo = localStorage.getItem('userInfo');
+    console.log('userInfo', userInfo);
+    
     setAccount(JSON.parse(userInfo as string));
   }, [])
   return (
-    <div>
-      <div>{account.nickname}</div>
-      <div>{account.age?.toString()}</div>
+    <div className='flex'>
+      <div className='p-[24px] h-[100vh] w-[420px] shadow flex'>
+        <Avatar size={64} src={account.avatar} icon={<UserOutlined />} />
+        <div>
+          <div>{account.nickname }</div>
+        </div>
+      </div>
+      <div className='flex-1'></div>
     </div>
   )
 }
